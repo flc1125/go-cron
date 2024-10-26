54326fa490a06ca42ad1a7a126a94184077a9cfe
Flc <four_leaf_clover@foxmail.com>
2024-10-26 18:06:46 +0800

    feat(cron): enable cron to support `context.Context`

--- option.go
+++ option.go
@@ -1,11 +1,19 @@
 package cron
 
 import (
+	"context"
 	"time"
 )
 
 // Option represents a modification to the default behavior of a Cron.
 type Option func(*Cron)
+
+// WithContext overrides the context used by the Cron.
+func WithContext(ctx context.Context) Option {
+	return func(c *Cron) {
+		c.ctx = ctx
+	}
+}
 
 // WithLocation overrides the timezone of the cron instance.
 func WithLocation(loc *time.Location) Option {
