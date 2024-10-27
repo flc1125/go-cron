782358fcb826b87c255dd688541776adf1631f42
Flc <four_leaf_clover@foxmail.com>
2024-10-27 21:35:04 +0800

    chore(example): add some examples

--- _example/middleware/recovery/main.go
+++ _example/middleware/recovery/main.go
@@ -1,20 +1,23 @@
-package recovery_test
+package main
 
 import (
 	"context"
+	"time"
 
 	"github.com/flc1125/go-cron/v4"
 	"github.com/flc1125/go-cron/v4/middleware/recovery"
 )
 
-func Example() {
+func main() {
 	c := cron.New()
 	c.Use(recovery.New())
 
-	_, _ = c.AddFunc("* * * * * ?", func(context.Context) error {
+	_, _ = c.AddFunc("* * * * * *", func(context.Context) error {
 		panic("YOLO")
 	})
 
 	c.Start()
 	defer c.Stop()
+
+	time.Sleep(2 * time.Second)
 }
