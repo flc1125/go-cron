8ac5a9084de735067512cf5084250e55acdcf9c2
Flc <four_leaf_clover@foxmail.com>
2024-10-27 21:18:20 +0800

    feat(entry): add `Entry` info in `ctx`

--- entry_test.go
+++ entry_test.go
@@ -3,6 +3,7 @@ package cron
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -37,4 +38,41 @@ func TestEntry_Context(t *testing.T) {
 			assert.NoError(t, entry.WrappedJob().Run(ctx))
 		})
 	}
+}
+
+func TestEntry_ContextUseCron(t *testing.T) {
+	cron := newWithSeconds()
+	var e1, e2 *Entry
+	_, err := cron.AddFunc("* * * * *", func(ctx context.Context) error {
+		entry, ok := EntryFromContext(ctx)
+		assert.True(t, ok)
+		assert.True(t, entry.Valid())
+		e1 = entry
+
+		t.Logf("entry id: %d", entry.ID)
+
+		return nil
+	})
+	assert.NoError(t, err)
+
+	_, err = cron.AddFunc("* * * * *", func(ctx context.Context) error {
+		entry, ok := EntryFromContext(ctx)
+		assert.True(t, ok)
+		assert.True(t, entry.Valid())
+		e2 = entry
+
+		t.Logf("entry id: %d", entry.ID)
+
+		return nil
+	})
+	assert.NoError(t, err)
+
+	cron.Start()
+	defer cron.Stop()
+
+	// wait for the job to run
+	time.Sleep(time.Second)
+
+	// ensure the entries are different
+	assert.NotEqual(t, e1.ID, e2.ID)
 }
