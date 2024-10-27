5511a889fdce81bf2a0afaa71bada785311e88a3
Flc <four_leaf_clover@foxmail.com>
2024-10-27 20:07:15 +0800

    feat(entry): add `Entry` info in `ctx`

--- entry_test.go
+++ entry_test.go
@@ -8,13 +8,33 @@ import (
 )
 
 func TestEntry_Context(t *testing.T) {
-	entry := newEntry(1, nil, JobFunc(func(ctx context.Context) error {
-		entry, ok := EntryFromContext(ctx)
-		assert.True(t, ok)
-		assert.Equal(t, entry.ID, EntryID(1))
+	tests := []struct {
+		name string
+		id   EntryID
+	}{
+		{"", 1},
+		{"", 2},
+	}
 
-		return nil
-	}))
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
 
-	assert.NoError(t, entry.WrappedJob().Run(context.Background()))
+			// non-existent entry
+			entry, ok := EntryFromContext(ctx)
+			assert.False(t, ok)
+			assert.Nil(t, entry)
+
+			// existent entry
+			entry = newEntry(tt.id, nil, JobFunc(func(ctx context.Context) error {
+				entry, ok := EntryFromContext(ctx)
+				assert.True(t, ok)
+				assert.Equal(t, entry.ID, tt.id)
+
+				return nil
+			}))
+
+			assert.NoError(t, entry.WrappedJob().Run(ctx))
+		})
+	}
 }
