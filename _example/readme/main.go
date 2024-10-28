c1e0a2d124966f86bd0784aba7e8ca37a8804763
Flc゛ <four_leaf_clover@foxmail.com>
2024-10-28 22:34:58 +0800

    docs(README): update README.md (#31)
    * docs(README): update README.md
    
    * docs(README): update README.md

---
+++ _example/readme/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+
+	"github.com/flc1125/go-cron/v4"
+	"github.com/flc1125/go-cron/v4/middleware/nooverlapping"
+	"github.com/flc1125/go-cron/v4/middleware/recovery"
+)
+
+func main() {
+	c := cron.New(
+		cron.WithSeconds(), // if you want to use seconds, you can use this option
+		cron.WithMiddleware(
+			recovery.New(),      // recover panic
+			nooverlapping.New(), // prevent job overlapping
+		),
+		// ... other options
+	)
+
+	// use middleware
+	c.Use(recovery.New(), nooverlapping.New()) // use middleware
+
+	// add job
+	entryID, _ := c.AddJob("* * * * * *", cron.JobFunc(func(ctx context.Context) error {
+		// do something
+		return nil
+	}))
+	_ = entryID
+
+	// add func
+	_, _ = c.AddFunc("* * * * * *", func(ctx context.Context) error {
+		// do something
+		return nil
+	})
+
+	// start cron
+	c.Start()
+
+	// stop cron
+	c.Stop()
+}
